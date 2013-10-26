8dfcefc5d262fc8975e6dff0836700ad64d1bb42
zerga1 <zerzagabor@gmail.com>
2013-10-26 20:33:44 +0200

    Init repo

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+RssApp::Application.initialize!
+
+#config.gem "pauldix-feedzirra", :lib => "feedzirra", :source => "http://gems.github.com"
