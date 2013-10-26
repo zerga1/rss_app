7e9221aef6723776bf8affa8824393393039680a
zerga1 <zerzagabor@gmail.com>
2013-10-27 00:44:59 +0200

    added update button

--- app/controllers/rss_controller.rb
+++ app/controllers/rss_controller.rb
@@ -1,4 +1,9 @@
 class RssController < ApplicationController
 	def index
 	end
+
+	def update
+		FeedEntry.update_from_feed("http://www.nb1.hu/rss.xml")
+		redirect_to root_url
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ RssApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'rss#index'
+  get '/update', to: 'rss#update'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
