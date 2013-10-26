69b0aa62fd4d2443ed5a015a348d6f2b01afb149
zerga1 <zerzagabor@gmail.com>
2013-10-27 00:58:24 +0200

    rss change

--- app/controllers/rss_controller.rb
+++ app/controllers/rss_controller.rb
@@ -3,7 +3,8 @@ class RssController < ApplicationController
 	end
 
 	def update
-		FeedEntry.update_from_feed("http://www.nb1.hu/rss.xml")
+		#FeedEntry.update_from_feed("http://www.nb1.hu/rss.xml")
+		FeedEntry.update_from_feed("http://www.nemzetisport.hu/static/rss_hirek.xml")
 		redirect_to root_url
 	end
 end
